fc73edc37d9bce466f2599db3330be4add9ad9e7
silkrow <si1krow@outlook.com>
2022-06-12 12:07:11 +0800

    vmprint done

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Added to print a page table.
+void vmprint(pagetable_t pagetable){
+	printf("page table %p\n", pagetable);	
+	for (int i = 0; i < 512; i++){
+		pte_t pte = pagetable[i];
+		if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){		
+			printf(" ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+			pagetable_t child1 = (pagetable_t)PTE2PA(pte);
+			for (int j = 0; j < 512; j++){
+				pte_t pte2 = child1[j];
+				if ((pte2 & PTE_V) && (pte2 & (PTE_R|PTE_W|PTE_X)) == 0){						
+					printf(" .. ..%d: pte %p pa %p\n", j, pte2, PTE2PA(pte2));
+					pagetable_t child2 = (pagetable_t)PTE2PA(pte2);
+					for (int k = 0; k < 512; k++){
+						pte_t pte3 = child2[k];
+						if ((pte3 & PTE_V) == 0){						
+							printf(" .. .. ..%d: pte %p pa %p\n", k, pte3, PTE2PA(pte3));
+						}
+					}
+				}
+			}
+		} 
+	}
+}
