7c91371b31db29436bd78de537c10ca819f60939
silkrow <si1krow@outlook.com>
2022-06-12 10:07:52 +0800

    speed up finished

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,16 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page. Added for speeding up the getpid() syscall,
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+	if(p->trapframe)
+    	kfree((void*)p->trapframe);
+	freeproc(p);
+	release(&p->lock);
+	return 0;
+  }
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -152,7 +162,10 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
   p->trapframe = 0;
+  p->usyscall = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -195,6 +208,15 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  
+  // Added for speeding up getpid() syscall.
+  if (mappages(pagetable, USYSCALL, PGSIZE, 
+	  (uint64)p->usyscall, PTE_R | PTE_U) < 0){
+	  	uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+	  	uvmunmap(pagetable, TRAPFRAME, 1, 0);
+		uvmfree(pagetable, 0);
+		return 0;
+  }
 
   return pagetable;
 }
@@ -206,6 +228,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
